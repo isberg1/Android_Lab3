d22054d36d8ece026aef34813dd75294727dd06d
isberg1 <alexajak@stud.ntnu.no>
2019-03-12 11:08:09 +0100

    Initial commit

---
+++ Assignment_lab3.md
@@ -0,0 +1,49 @@
+# Lab 03: Throwing a virtual ball upwords
+
+## The idea
+
+Create an application that allows the user to throw a virtual (non existent) ball upwards. The app can consist of one main activity and one for Settings. The app should use accelerometer to detect a "throw event", which is an acceleration above certain sensitivity threshold. This "throw event" will initiate a following sequence of events:
+
+   * the maximum acceleration will be used as the initial speed of the ball traveling upwards. Let's say, the top acceleration was equal to double the earth gravity (19.6m/s2).
+   * we will assume that the ball is then traveling upwards with the speed 19.6m/s, and that the gravity is pulling it down (slowing it down) 9.8m/s2.
+   * Roughly speaking, the ball, after 2s, should reach the highest point, and stop for a brief moment.
+   * At that top moment, the phone should play a sound indicating that the ball reached the highest point.
+   * After that, the ball should fall down exactly the same amount of time, as it was traveling upwards (yes, we are ignoring the air resistance).
+   * When the ball travels upwards, you should display a counter, showing, how many meters the ball has travelled up before reaching the "stop" point. This would be the "SCORE" for a given throw.
+
+The above should be recalculated for the actual top acceleration reached during the "throw" movement of the phone.
+
+To calculate the actual acceleration, you should use simple equation: `ACC = sqrt(x*x + y*y + z*z) - EarthGravity` where `x, y, z` are the acceleration readings from the accelerometer, and `EarthGravity` is the constant representing EarthGravity.  Therefore, a phone placed flat on a floor should provide you value of ACC close to 0.
+
+
+## Preferences
+
+The user should be able to specify in the preferences the sensitivity threshold (use slider) of what is the minimum value of acceleration ACC that would indicate a THROW event.  Anything below that MIN_ACC value will be ignored, and not considered as throw.
+
+
+## Extras
+
+To make the app more fun, you can play a sound with decreasing frequency on the ball movement UP, and a sound with increasing frequency on the way of the ball down.
+
+You can also keep the "high score" list of the best "throws" of the ball (the highest distance reached).
+
+You can provide haptic feedback to the hand holding the phone, when the ball leaves the hand (the highest acceleration reached), and then, again, when the ball comes back down to the same location from where it has been thrown. This should feel as if the ball leaves the hand, and then comes back.
+
+
+
+# Checklist
+
+* [ ] The git repository URL is correctly provided, such that command works: `git clone <url> `
+* [ ] The code is well, logically organised and structured into appropriate classes. Everything should be in a single package.
+* [ ] The app has been user tested with someone other than the author.
+* [ ] The user can go to Preferences and set the MIN_ACC value (sensitivity).
+* [ ] The app plays sounds on the ball highest point.
+* [ ] The app records the highest point reached by the ball.
+* [ ] Share your sensitivity constant as well as the size of the sliding window with others.
+
+
+## Hints
+
+The equation for calculating the temporary acceleration at a specific time point is provided above. To know if the acceleration is the "highest", you need to use a technique called "sliding window". What it means, is that you will calculate the ACC for example for 20 accelerometer readings, and then, subsequently, keep adding new readings as they are provided from the accelerometer. If the highest temporary reading "slides out" of the 20 readings, you consider it the highest overall. This means, the ball will "start flying" actually 20 timeslots after the event happened. You can tweak the size of the window for the app not to feel laggy. If the accelerometer provides you data with frequency 100Hz, 20 readings will take 1/5th of a second. Play with the constants, to achieve the most natural feel of the game.
+
+
---
+++ app/src/main/java/com/e/android_lab3/MainActivity.java
@@ -0,0 +1,292 @@
+package com.e.android_lab3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private static final String TAG = "MainActivity";
+
+    TextView heightTextView, accTextview, accSlidinWindow, timeInAir, distanseTewxtview;
+    SensorManager sensorManager;
+    Sensor accelerometer;
+    Button reset;
+
+    int threshold = 5;
+    double g = 9.8;
+    double oldMax;
+    double time;
+    double distance;
+    List<Double> slidingWindow = new ArrayList<>();
+    LinearLayout animationSpace;
+    AnimatorSet animatorSet = new AnimatorSet();
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        accTextview = findViewById(R.id.acc);
+
+        accSlidinWindow = findViewById(R.id.acc_sliding_window);
+        timeInAir= findViewById(R.id.time_in_air);
+        distanseTewxtview = findViewById(R.id.distance);
+        animationSpace = findViewById(R.id.animation_space);
+        reset = findViewById(R.id.reset_button);
+        heightTextView = findViewById(R.id.height_textView);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer,SensorManager.SENSOR_DELAY_GAME);
+
+
+        mp = MediaPlayer.create(this, R.raw.ping);
+
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (animatorSet.isRunning()) {
+            reset.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            return;
+        }
+        reset.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+        float xx = event.values[0];
+        float yy = event.values[1];
+        float zz = event.values[2];
+        double acc = Math.sqrt(xx*xx +yy*yy+zz*zz) - g;
+
+        if (acc < threshold) {
+            acc = 0;
+        }
+
+        accTextview.setText("Acceleration:  " + format(acc));
+
+
+        slidingWindow.add(acc);
+        if (slidingWindow.size() > 10){
+            slidingWindow.remove(0);
+            double newMax = Collections.max(slidingWindow);
+            if (newMax > oldMax){
+                oldMax = newMax;
+                accSlidinWindow.setText("Sliding window max:" + format(oldMax));
+
+                time = oldMax/g;
+                distance = 0.5 *g*time*time;
+
+                timeInAir.setText("Time: " + format(time));
+                distanseTewxtview.setText("Distance: " + format(distance));
+                startAnimation(time, distance);
+            }
+
+        }
+
+    }
+
+    public String format(double num) {
+        String returnNum = null;
+        try {
+            returnNum = String.format("%.2f",num);
+        } catch (Exception e) {
+            Log.d(TAG, "format: unable to convert double to string");
+            e.printStackTrace();
+        }
+        return returnNum;
+    }
+
+    private void startAnimation(final double time, final double distance) {
+
+        if (animatorSet.isRunning()) {
+            return;
+        }
+
+        Button button = findViewById(R.id.tulleknapp);
+
+        float layoutHeight = animationSpace.getHeight();
+        float maxHeightMeter = (float) (distance/40);
+        float screenHeight = layoutHeight * maxHeightMeter;
+        int animationTime = (int) (time * 1000);
+
+        final Animator upAnimation = ObjectAnimator
+                .ofFloat(button, View.TRANSLATION_Y, 0f, -screenHeight)
+                .setDuration(animationTime);
+
+
+        final Animator downAnimation = ObjectAnimator
+                .ofFloat(button, View.TRANSLATION_Y, -screenHeight, 0f)
+                .setDuration(animationTime);
+
+        upAnimation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                updateHeight(time, distance);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                playSound();
+                heightTextView.setText("Height: " + format(distance));
+            }
+        });
+
+
+
+        animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(
+                upAnimation,
+                downAnimation
+        );
+        animatorSet.start();
+        slidingWindow.clear();
+        oldMax = threshold;
+
+
+
+        // resetSlidingWindowMax();
+    }
+
+    int updateFrequency = 0;
+    int count = 0;
+    private void updateHeight(double time, double distance) {
+        updateFrequency = 21;
+
+        if(distance < 10) {
+            updateFrequency = 12;
+        } else if (distance < 20) {
+            updateFrequency = 15;
+        } else if (distance < 30) {
+            updateFrequency = 18;
+        }
+
+        final double distanceInterval = distance / updateFrequency;
+        final double timeInterval = time / updateFrequency;
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for ( int i = updateFrequency; i > 0; i--) {
+                    final int finalI = i;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            heightTextView.setText("Height: " + format(distanceInterval* count++));
+
+                        }
+                    });
+
+                    try {
+                        Thread.sleep((long) (timeInterval * 1000));
+                    } catch (InterruptedException e) {
+                        Log.d(TAG, "run: unable to sleep");
+                        e.printStackTrace();
+                    }
+
+
+                }
+                updateFrequency = 0;
+                count = 0;
+            }
+        }).start();
+
+
+
+    }
+
+    private void playSound() {
+        try {
+            if (mp.isPlaying()) {
+                mp.stop();
+                mp.release();
+                mp = MediaPlayer.create(getBaseContext(), R.raw.ping);
+            } mp.start();
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+
+
+    }
+
+    public void resetSlidingWindowMax(View view) {
+        oldMax = 0;
+        time = 0;
+        distance = 0;
+
+        accSlidinWindow.setText("Sliding window max:" + format(oldMax));
+        slidingWindow.clear();
+        timeInAir.setText(" ");
+        timeInAir.setText("Time: " + format(time));
+        distanseTewxtview.setText("Distance: " + format(distance));
+
+
+
+    }
+
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+/*
+physics source: https://www.sausd.us/cms/lib5/ca01000471/centricity/moduleinstance/8024/physics_ii.pdf
+*/
